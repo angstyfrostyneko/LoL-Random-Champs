5d6dda3115f7e87698d659875314e33be45e7367
Millie <angstyfrostyneko@proton.me>
2024-03-18 21:33:53 +0200

    added argument to ignore lanes

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,15 @@
 use rand::prelude::*;
+use std::env;
 use std::fs;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let mut ignore_lanes = false;
+
+    if args.len() > 1 && args[1] == "-ignorelanes" {
+        ignore_lanes = true;
+    }
+
     let lane_names = ["Top", "Jungle", "Middle", "Bot", "Support"];
 
     let mut top: Vec<String> = Vec::new();
@@ -20,6 +28,7 @@ fn main() {
 
     let file: Vec<_> = file.split('\n').collect();
 
+    // reading all champs and putting on lanes
     let mut vectors = vec![&mut top, &mut jng, &mut mid, &mut bot, &mut sup];
 
     for (index, vector) in vectors.iter_mut().enumerate() {
@@ -31,6 +40,10 @@ fn main() {
             .for_each(|x| vector.push(x.to_string()));
     }
 
+    if ignore_lanes {
+        vectors.shuffle(&mut rng);
+    }
+
     for (index, vector) in vectors.iter_mut().enumerate() {
         loop {
             let pick = vector.choose(&mut rng).unwrap().to_string();
@@ -39,6 +52,14 @@ fn main() {
                 break;
             };
         }
-        println!("{}: {}", lane_names[index], selected[index].replace("+", " "));
+        println!(
+            "{}: {}",
+            lane_names[index],
+            selected[index].replace("+", " ")
+        );
+    }
+
+    if !ignore_lanes {
+        println!("Pro tip: if you add \"-ignorelanes\" at the end of the command it will give champs regardless of the lane they're on!");
     }
 }
